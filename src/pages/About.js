6de9a2289181486098a1ef0224ac362a9fa1315b
Mister Tanatswa <tanatswasingano@gmail.com>
2022-08-22 16:00:28 -0400

    Create assets folder and edit About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import './../assets/styles/about.css'
 
 
 function About() {
   return (
-    <h1>THIS IS THE ABOUT PAGE</h1>
-  );  
+    <div>
+      <div className="container">
+
+        <h1>About Me</h1>
+          <img src="https://ca.slack-edge.com/TUFNTT7ND-UURHQ9VPE-194f27264248-512" alt="Guillermo" />
+
+          <div className="row">
+            <p>
+              Hello! My name's Tanatswa, im currently a student at Macomb Community College going for an associates degree in IT.
+            </p>
+
+            <p>
+             My interest in web dev
+            </p>
+
+            <p>
+              In my free time, .
+            </p>
+          </div>
+
+
+
+      </div>
+    </div>
+  );
 }
 
 export default About;
